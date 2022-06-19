7affae7d3723bdd8969b0ba8ce3a9e9f62eccd7a
tomatte <daniel.brandao2000@gmail.com>
2022-06-19 16:13:08 +0200

    strnstr fixed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,39 +6,12 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 04:48:39 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/06/06 05:20:54 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/06/19 16:11:22 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (!n)
-		return (0);
-	while (s1[i] || s2[i])
-	{
-		if (i + 1 == n)
-			break ;
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
-	}
-	return (s1[i] - s2[i]);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i++])
-		;
-	return (i - 1);
-}
+#include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -49,7 +22,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	little_len = ft_strlen(little);
 	if (!*little)
 		return ((char *) big);
-	while (i + little_len < len && big[i])
+	while (i + little_len <= len && big[i])
 	{
 		if (!ft_strncmp(&big[i], little, little_len))
 			return ((char *)(&big[i]));
@@ -64,6 +37,9 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 int	main(void)
 {
 	char	big[] = "picolé de suquinho é muito refrescante";
+	char haystack[30] = "aaabcabcd";
+	
+	printf("mine: %s\n", ft_strnstr(haystack, "aaabc", 5));
 	
 	printf("mine: %s\n", ft_strnstr(big, "co", 20));
 	printf("orig: %s\n\n", strnstr(big, "co", 20));
@@ -91,6 +67,7 @@ int	main(void)
 
 	printf("mine: %s\n", ft_strnstr("", "", 100));
 	printf("orig: %s\n\n", strnstr("", "", 100));
+
 	return (0);
 }
 */
