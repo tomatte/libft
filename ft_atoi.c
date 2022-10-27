4778318575dab89fde0d69e4468f90511e13cc92
tomatte <daniel.brandao2000@gmail.com>
2022-10-27 18:14:12 -0300

    replaced some hard code with functions in ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 05:54:21 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/06/26 14:03:10 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/10/27 18:11:10 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *nptr)
 {
@@ -17,16 +19,13 @@ int	ft_atoi(const char *nptr)
 
 	sign = 1;
 	num = 0;
-	while (*nptr == ' ' || *nptr == '\f' || *nptr == '\n'
-		|| *nptr == '\r' || *nptr == '\t' || *nptr == '\v')
-	{
+	while (ft_isspace(*nptr))
 		nptr++;
-	}
 	if (*nptr == '-')
 		sign *= -1;
 	if (*nptr == '-' || *nptr == '+')
 		nptr++;
-	while (*nptr > 47 && *nptr < 58)
+	while (ft_isdigit(*nptr))
 	{
 		num = num * 10 + (*nptr - 48);
 		nptr++;
